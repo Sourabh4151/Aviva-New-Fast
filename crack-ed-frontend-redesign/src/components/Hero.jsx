9d507391b0dabd6353404de3b822168a87378dd2
Sourabh4151 <sourabhtayal212@gmail.com>
2026-06-16 14:49:15 +0530

    Demand state updated

--- crack-ed-frontend-redesign/src/components/Hero.jsx
+++ crack-ed-frontend-redesign/src/components/Hero.jsx
@@ -9,7 +9,17 @@ import crackedLogo from "../assets/crack-ed.svg";
 
 import stateCities from "../data/indian_state_cities.json";
 
-const INDIAN_STATES = Object.keys(stateCities).sort((a, b) => a.localeCompare(b));
+const INDIAN_STATES = [
+  "Andhra Pradesh",
+  "Delhi",
+  "Haryana",
+  "Karnataka",
+  "Madhya Pradesh",
+  "Punjab",
+  "Rajasthan",
+  "Telangana",
+  "Uttar Pradesh",
+].filter((state) => Array.isArray(stateCities[state]));
 const MAX_FILTERED_OPTIONS = 100;
 
 function filterByQuery(options, query) {
